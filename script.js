d60efd3c7cffc34b65a418e3d0bbeec11bd5f327
moenova <harryshanghai190@gmail.com>
2019-06-27 23:04:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,32 +4,86 @@
 const log = console.log
 
 
-document.querySelector("body button").addEventListener("click", fun)
 
 
-function fun(e) {
-    e.preventDefault();
-    
+function getRation() {
     const fixGoldNum = document.querySelector("body table #fixGoldNum").value
     const exp = document.querySelector("body table #exp").value
     const gold = document.querySelector("body table #gold").value
     const cash = document.querySelector("body table #cash").value
     
-    //log(exp.value)
-    //log(gold.value)
-    //log(cash.value)
+    const jade = document.querySelector("body table #jade").value
+    const ration_limit = document.querySelector("body table #ration_limit").value
+    
+    
+    /*
+    log(fixGoldNum)
+    log(ration_limit)
+    log(exp)
+    log(gold)
+    log(cash)
+    log(jade)
+    */
+    
+    
+    
     const factor = 100
-    const min_raw = (exp*factor)/259.1 + Math.min(gold*factor+fixGoldNum*500,cash*factor)/250
+    const rock2ration =5.43;
     
-    const Rmin =Math.round(min_raw * 10) / 10
-    log(Rmin)
-    document.querySelector("body table #Rmin").innerText = Rmin
+    const exp2ration = (exp*factor)/259.1;
+    
+    const gold2ration_min = Math.min(gold*factor+fixGoldNum*500,cash*factor)/250
+    const gold2ration_ave = (gold*factor,cash*factor)/500
+    
+    const jade2ration = (ration_limit/18 - 2*rock2ration - 6.4)*(jade/10)
+    
+    //log("jade:",jade2ration)
     
     
-    const ave_raw = (exp*factor)/259.1 + (gold*factor,cash*factor)/2/250
+    const min_raw =   exp2ration  + gold2ration_min + jade2ration   
+    const ave_raw =   exp2ration  + gold2ration_ave + jade2ration
     
-    const Rave =Math.round(ave_raw * 10) / 10
-    log(Rave)
-    document.querySelector("body table #Rave").innerText = Rave
+          
+    document.querySelector("body table #Rmin").innerText = getR(min_raw)
+    
+    document.querySelector("body table #Rave").innerText = getR(ave_raw)
+    
+    
+    
 }
 
+
+function getR(value){
+    const res = Math.round(value * 10) / 10
+    //log(res)
+    return res
+}
+
+
+function getBalanceRD(){
+    const Brock = document.querySelector("body table #Brock").value
+    const Bdevice = document.querySelector("body table #Bdevice").value
+    //log(Brock)
+    //log(Bdevice)
+    document.querySelector("body table #BcashRock").value = Brock*800    
+    document.querySelector("body table #BcashDevice").value = Bdevice*1000
+    
+    document.querySelector("body table #BcashTotal").innerText = Brock*800  + Bdevice*1000
+    document.querySelector("#Bjade").innerText = Brock*5 +Bdevice*10
+    
+}
+
+
+function getBalanceCash(){
+    const BcashRock = parseInt(document.querySelector("body table #BcashRock").value)
+    const BcashDevice = parseInt(document.querySelector("body table #BcashDevice").value)
+    
+    document.querySelector("body table #BcashTotal").innerText = BcashRock + BcashDevice
+    
+    document.querySelector("body table #Brock").value = BcashRock/800
+    document.querySelector("body table #Bdevice").value =BcashDevice/1000
+    
+    document.querySelector("#Bjade").innerText = BcashRock/160 + BcashDevice/100
+    
+
+}
